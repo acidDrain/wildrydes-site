284ec8934b0891d05f449ce8af1045b3408e98a8
James Grow <jmgrow@gmail.com>
2022-03-08 18:37:44 -0600

    Updated the cognito auth config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_WOc8AFWQp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ov8o568b6ll6i0ot8r5lea32l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
